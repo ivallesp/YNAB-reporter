78a6ea47b6b79e2bf6a913c5bb9b5d859f55701d
Valles Perez <ivallesp@38f9d3dad149.ant.amazon.com>
2020-06-08 02:36:11 +0200

    Add main script with argparse

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
+import os
 import argparse
 import logging.config
-from src.api import update_ynab
 from src.paths import get_log_config_filepath
+from src.reporting import MONTHS, generate_latex_report
 
 logging.config.fileConfig(get_log_config_filepath(), disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="N26 to YNAB bridge. Run the program to download the transactions "
-        "from the N26 account and upload the into the YNAB budget account. "
-        "Example: python main.py -a my_account_name"
+        description="YNAB personal finance PDF report generation. Run the program "
+        "to connect to a YNAB account, download all the transactions available in "
+        "the specified report and sumarise it in a monthly PDF document."
     )
 
     parser.add_argument(
-        "-a",
+        "-m",
         action="store",
-        dest="account",
+        dest="month",
         required=True,
-        help="Name of the account to update. Has to be defined in config/n26.toml",
+        help="Month to generate a summary of",
+    )
+
+    parser.add_argument(
+        "-y",
+        action="store",
+        dest="year",
+        required=True,
+        help="Year to generate a summary of",
     )
 
     results = parser.parse_args()
 
     # Run the update process
-    logger.info(f"Requested 💰 YNAB update for account name: {results.account}")
-    update_ynab(results.account)
-    logger.info(f"YNAB update performed successfully! 🎉🎊🥳")
+    logger.info(
+        f"Requested 💰 LaTeX report generation for month "
+        f"{MONTHS[int(results.month)]} and year {int(results.year)}"
+    )
+    generate_latex_report(year=int(results.year), month=int(results.month))
+    os.system("cd assets && pdflatex report.tex")
+    logger.info(f"YNAB report generated successfully! 🎉🎊🥳")
